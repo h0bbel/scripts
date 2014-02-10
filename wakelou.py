6d88558f2b6bc8386fd708b218040300d5156768
h0bbel <christian@dribe.net>
2014-02-10 14:05:24 +0100

    Small "bug fix".

--- wakelou.py
+++ wakelou.py
@@ -4,4 +4,4 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.sendto('\xff'*6 + '\x00\x23\xEA\xAD\xC3\xEA'*16, ('192.168.5.255', 80))
-print "Magic Packet sent to ´lou´ - Is it alive?";
+print "Magic Packet sent to lou - Is it alive?";
